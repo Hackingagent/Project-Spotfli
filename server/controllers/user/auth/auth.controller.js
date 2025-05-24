ffa38ddd44b9f3ecda73377b04ae3e924ddca23e
Ngulefac Terence <telluriumtriol@gmail.com>
2025-05-24 11:18:58 +0100

    admin and user models created

---
+++ server/controllers/user/auth/auth.controller.js
@@ -0,0 +1,7 @@
+import User  from "../../../models/user.model.js";
+import Admin from "../../../models/admin.model.js";
+import mongoose from "mongoose";
+
+export const registerUser = async(req, res) => {
+    console.log(req);
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
 const app = express();
 // fS2p1S0VCAvKXwK4
+
+app.use(express.json());
+
+app.use("api/user", userRoutes);
+
+// Error handling middleware
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+});
+
 app.listen('5000', ()=>{
     connectDB()
     console.log('Server is Running');
