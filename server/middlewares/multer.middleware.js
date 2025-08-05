a56a5c1f1cda6bc4d2362c00666a970741a7b5fc
Ngulefac Terence <telluriumtriol@gmail.com>
2025-08-05 20:53:06 +0100

    Updated file upload for Terence

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,19 +1,32 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.join(__dirname, '../uploads/rooms');
+
+if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir, {recursive: true});
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/rooms/');
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const ext = path.extname(file.originalname);
+    cb(null, 'images-' + uniqueSuffix + ext);
+  }
 });
 
 const upload = multer({ 
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+    limits: { fileSize: 10 * 1024 * 1024 }, // 5MB limit
     fileFilter: (req, file, cb) => {
         const filetypes = /jpeg|jpg|png/;
         const mimetype = filetypes.test(file.mimetype);
